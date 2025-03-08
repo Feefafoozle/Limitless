f548d37c2f04783dfefd863eccba86b432d3581e
Astrallion <astrallionstudios@gmail.com>
2025-03-08 22:46:42 +0000

    Add inheritance with parent Weapon object

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [Header("Basic Stats")]
     [SerializeField] protected float Damage;
     [SerializeField] protected float ReloadTime;
     [SerializeField] protected float UnloadTime;
@@ -11,8 +12,27 @@ public class Weapon : MonoBehaviour
     protected int Ammo;
     protected float CurrReloadTime;
     protected float CurrUnloadTime;
+    bool IsReloading;
     
     protected List<int> ReturnAmmoStats() {
         return new List<int> {Ammo, MaxAmmo};
+    }
+
+    protected void CheckReload(bool ReloadInput) {
+        if(!IsReloading && ReloadInput) {
+            Reload();
+        }
+
+        if(IsReloading) {
+            if(CurrReloadTime + ReloadTime <= Time.time) {
+                Ammo = MaxAmmo;
+                IsReloading = false;
+            }
+        }
+    }
+
+    protected void Reload() {
+        CurrReloadTime = Time.time;
+        IsReloading = true;
     }
  }
